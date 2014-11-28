d8456fb7668849efa7ad4713ef06b9321bfea4e5
justintanner <justinwtanner@gmail.com>
2014-11-28 18:49:29 +1100

    removing junk'

--- spec/lib/column_pack_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe ColumnPack do
-  it "should initalize" do
-    column_pack = ColumnPack.new(3)
-    expect(column_pack.class).to eq ColumnPack
-  end
-end
--- spec/spec_helper.rb
+++
@@ -1,3 +0,0 @@
-require 'rubygems'
-require 'action_view'
-require 'action_view/helpers/column_pack'
