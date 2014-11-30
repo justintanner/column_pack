46b11052448ca000b5167aea903f92b49d9fcc1c
justintanner <justinwtanner@gmail.com>
2014-11-30 20:29:30 +1100

    adding asset

--- lib/column_pack.rb
+++ lib/column_pack.rb
@@ -1,1 +1,9 @@
 require 'column_pack/railtie' if defined? Rails
+
+module ColumnPack
+  module Rails
+    class Engine < ::Rails::Engine
+      # Get rails to add app, lib, vendor to load path
+    end
+  end
+end
