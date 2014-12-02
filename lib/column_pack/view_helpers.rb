0ccdd2f8cf351251db8d9c4f11a4a51c15fb6f42
justintanner <justinwtanner@gmail.com>
2014-12-02 17:25:20 +1100

    testing

--- lib/column_pack/view_helpers.rb
+++ lib/column_pack/view_helpers.rb
@@ -9,5 +9,21 @@ module ColumnPack
       cp.render
     end
 
+    def p1
+      @test = String.new
+      yield(@test)
+    end
+
+    def p2(test, height, content = nil, &block)
+      if block_given?
+        test.concat capture(&block)
+      else
+        test.concat content
+      end
+    end
+
+    def p3
+      @test.html_safe
+    end
   end
 end
